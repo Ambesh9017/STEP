0b9bef5c9b35462cbbc9ca4d86571326ad164ea9
singhambesh9018@gmail.com <Ambesh9017>
2026-03-02 14:48:55 +0530

    UC3

--- 4th Sem/PalindromeCheckerApp.java
+++ 4th Sem/PalindromeCheckerApp.java
@@ -1,15 +1,15 @@
-public class PalindromeCheckerApp {
-
-    public static void main(String[] args) {
-
+public class PalindromeCheckerApp
+{
+    public static void main(String[] args)
+    {
         System.out.println("Palindrome Checker App");
-        System.out.println("UC2: Print a Hardcoded Palindrome Result");
+        System.out.println("UC3: Palindrome Check Using String Reverse");
         System.out.println();
-
-        String word = "madam";
-
-        String reversed = new StringBuilder(word).reverse().toString();
-
+        String word = "level";
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
         if (word.equals(reversed)) {
             System.out.println(word + " is a Palindrome.");
         } else {
